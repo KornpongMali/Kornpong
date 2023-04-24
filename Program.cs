ce745148903985666c157776242def0d1d4adab7
KornpongMali <122720394+KornpongMali@users.noreply.github.com>
2023-04-24 10:43:29 +0700

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,24 @@
+class program{
+    static void Main(string[] args){
+        int[] num = new int[10];
+        for (int i = 0; i < 10; ++i){
+            num[i] =
+int.Parse(Console.ReadLine());
+        }
+        for (int i = 9; i >= 0; --i) {
+            Console.WriteLine(num[i]);
+            int size = int.Parse(Console.ReadLine());
+            float[] numm = new float[size];
+            // input data to array
+            for (int u = 0; i < size; i++){
+                numm[i] = float.Parse(Console.ReadLine());
+            }
+            //sort
+            for(int u = size - 1; i > 0; i--){ // size array
+            for (int j = 0; j < i; j++){
+                
+            }
+          }
+        }
+    }
+}
